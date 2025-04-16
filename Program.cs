7e5f032f5e0f54217b209997edf1152b3ab22cb9
jay <jaypearsonjr@live.com>
2025-04-16 13:47:54 -0400

    initial base project

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CombineLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> letters = new List<string>
+            {
+                "a", "b", "c", "d"
+            };
+
+            List<int> numbers = new List<int>
+            {
+                1, 2, 3, 4
+            };
+
+            List<object> characters = new List<object>();
+
+            for (int i = 0; i < letters.Count && i < numbers.Count; i++)
+            {
+                characters.Add(letters[i]);
+                characters.Add(numbers[i]);
+            }
+
+            foreach (object character in characters)
+            {
+                Console.WriteLine(character);                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
